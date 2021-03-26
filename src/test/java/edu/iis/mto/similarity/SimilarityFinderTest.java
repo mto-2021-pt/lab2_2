3c799908334b9611abb4812c9a5ed45f3aaec1df
Jakub Wlaźlak <222611@edu.p.lodz.pl>
2021-03-26 17:13:41 +0100

    Dodanie testu sprawdzającego zachowanie programu w przypadku pustych sekwencji

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,27 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testWhenEmptySequences() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                return SearchResult.builder().build();
+            }
+        });
+
+        int firstTab[] = new int[0];
+        int secondTab[] = new int[0];
+
+        double result = similarityFinder.calculateJackardSimilarity(firstTab, secondTab);
+        Assertions.assertEquals(1, result);
     }
 
 }
