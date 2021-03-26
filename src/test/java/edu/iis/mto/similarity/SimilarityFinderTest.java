44c8910163aed60ce12755b4afe61be69f6fdfcf
Jakub Wlaźlak <222611@edu.p.lodz.pl>
2021-03-26 17:46:33 +0100

    Dodanie testu sprawdzającego zachowanie programu w przypadku takich samych sekwencji

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -25,4 +25,35 @@ class SimilarityFinderTest {
         Assertions.assertEquals(1, result);
     }
 
+    @Test
+    void testWithTheSameSequences() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                SearchResult searchResult = null;
+                if (elem == 13) {
+                    searchResult = SearchResult.builder().withPosition(0).withFound(true)
+                            .build();
+                } else if (elem == 4) {
+                    searchResult = SearchResult.builder().withPosition(1).withFound(true)
+                            .build();
+                }
+                else if (elem == 29){
+                    searchResult = SearchResult.builder().withPosition(2).withFound(true)
+                            .build();
+                }
+                else if (elem == 16){
+                    searchResult = SearchResult.builder().withPosition(3).withFound(true)
+                            .build();
+                }
+                return searchResult;
+            }
+        });
+
+        int tab[] = {13, 4, 29, 16};
+        int tab2[] = {13, 4, 29, 16};
+
+        double result = similarityFinder.calculateJackardSimilarity(tab, tab2);
+        Assertions.assertEquals(1 , result);
+    }
 }
