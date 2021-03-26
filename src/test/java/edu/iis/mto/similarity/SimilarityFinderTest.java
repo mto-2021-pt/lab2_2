a3a33ebd44790a40755b0bf25394e8d51c792362
Julia <222596@edu.p.lodz.pl>
2021-03-27 00:02:42 +0100

    Test z 5-krotnym wywołaniem metody

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
@@ -52,6 +53,25 @@ class SimilarityFinderTest {
         int[] seq2 = {9,8,7};
 
         assertEquals(0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    SequenceSearcher sequenceSearcher = new SequenceSearcher() {
+        private int counter = 0;
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            counter++;
+            return SearchResult.builder().withFound(true).build();
+        }
+    };
+
+    @Test
+    public void testingForFiveInvokes() throws NoSuchFieldException, IllegalAccessException {
+        int [] seq1={1,2,3,4};
+        int [] seq2={66,77,88,99,99};
+
+        SimilarityFinder similarityFinder= new SimilarityFinder(sequenceSearcher);
+        similarityFinder.calculateJackardSimilarity(seq2,seq1);
+        assertEquals(5, sequenceSearcher.getClass().getDeclaredField("counter").getInt(sequenceSearcher));
     }
 
 }
