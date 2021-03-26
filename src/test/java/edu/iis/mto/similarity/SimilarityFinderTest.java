b222341b9912cdfd718c591083c93b05fd886f14
NickiMina <olkasia@poczta.one.pl>
2021-03-26 17:20:47 +0100

    utworzenie testu dla dwoch takich samych jednoelementowych tablic

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,31 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+
+    SequenceSearcher sequenceSearcherTrue = new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+             SearchResult.Builder builder = SearchResult.builder();
+             builder.withFound(true);
+             return builder.build();
+        }
+    };
+
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test1() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherTrue);
+
+        double result = similarityFinder.calculateJackardSimilarity(new int[]{3},new int[]{3});
+        assertEquals(1,result);
     }
 
 }
