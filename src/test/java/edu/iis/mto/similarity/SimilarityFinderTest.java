1ae9deeaf9a30c2ec0a9482a1b02d6194ecbe5cd
Stasiak.Jacek <222579@edu.p.lodz.pl>
2021-03-26 16:57:43 +0100

    Dodano testy stanu

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,68 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class SimilarityFinderTest {
 
+    private SearchResult sr_not_found = SearchResult.builder().withFound(false).build();
+    private SearchResult sr_found = SearchResult.builder().withFound(true).build();
+    private double delta = 1e-6;
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test1() {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {2, 3, 4, 5};
+
+        int[] search_tests = {
+                0, 1, 1, 1
+        };
+
+        SearchResult[] search_results = Arrays.stream(search_tests).mapToObj(e -> e == 0 ? sr_not_found : sr_found).toArray(SearchResult[]::new);
+
+        SimilarityFinder finder = new SimilarityFinder(new StateTestSearcher(search_results));
+        double result = finder.calculateJackardSimilarity(arr1, arr2);
+        double expected = 0.6;
+
+        assertEquals(expected, result, delta);
+    }
+
+
+    @Test
+    void test2() {
+        int[] arr = {};
+
+        SearchResult[] search_results = {};
+
+        SimilarityFinder finder = new SimilarityFinder(new StateTestSearcher(search_results));
+        double result = finder.calculateJackardSimilarity(arr, arr);
+        double expected = 1.0;
+
+        assertEquals(expected, result, delta);
+    }
+
+
+    @Test
+    void test3() {
+        int[] arr1 = {1, 3, 9, 18};
+        int[] arr2 = {3, 9, 100, 200, 300, 1000};
+
+        int[] search_tests = {
+                0, 1, 1, 0
+        };
+
+        SearchResult[] search_results = Arrays.stream(search_tests).mapToObj(e -> e == 0 ? sr_not_found : sr_found).toArray(SearchResult[]::new);
+
+        SimilarityFinder finder = new SimilarityFinder(new StateTestSearcher(search_results));
+        double result = finder.calculateJackardSimilarity(arr1, arr2);
+        double expected = 0.25;
+
+        assertEquals(expected, result, delta);
     }
 
 }
