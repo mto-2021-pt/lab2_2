ffd1eb74dfa4d13d49dfbc623d0027a4d5c77b99
rpanimation <rpanimation@icloud.com>
2021-03-26 19:20:59 +0100

    Added state and behaviour tests.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -5,147 +5,103 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import edu.iis.mto.searcher.SearchResult;
 import edu.iis.mto.searcher.SequenceSearcher;
+import edu.iis.mto.searcher.TestSequenceSearcher;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
-    final SequenceSearcher seqS = (elem, sequence) -> {
-        for (int i = 0; i < sequence.length; ++i) {
-            if (elem == sequence[i]) {
-                return SearchResult.builder().withPosition(i).withFound(true).build();
-            }
+    @Test
+    void jaccardIndexTestArraysWithTheSameSize() {
+        int[] a = {0, 1, 2, 3};
+        int[] b = {1, 3, 4, 5};
+        int[] searchResults = {0, 1, 0, 0};
+        double unique = 7.0;
+        double shared = 1.0;
+        double exp_res = shared/unique;
+
+        SimilarityFinder sf = new SimilarityFinder(new TestSequenceSearcher(searchResults));
+        double res = sf.calculateJackardSimilarity(a, b);
+        assertEquals(exp_res, res, 0.0001f);
+    }
+    @Test
+    void jaccardIndexTestArraysWithDifferentSize() {
+        int[] a = {0, 1, 2, 3, 4, 5};
+        int[] b = {1, 3, 4, 5, 9, 22, 11, 33};
+        int[] searchResults = {0, 1, 0, 1, 1, 1};
+        double unique = 10.0;
+        double shared = 4.0;
+        double exp_res = shared/unique;
+
+        SimilarityFinder sf = new SimilarityFinder(new TestSequenceSearcher(searchResults));
+        double res = sf.calculateJackardSimilarity(a, b);
+        assertEquals(exp_res, res, 0.0001f);
+    }
+    @Test
+    void jaccardIndexTestSameArrays() {
+        int[] a = {0, 1, 3, 4, 5};
+        int[] b = {0, 1, 3, 4, 5};
+        int[] searchResults = {1, 1, 1, 1, 1};
+        double unique = 5.0;
+        double shared = 5.0;
+        double exp_res = shared/unique;
+
+        SimilarityFinder sf = new SimilarityFinder(new TestSequenceSearcher(searchResults));
+        double res = sf.calculateJackardSimilarity(a, b);
+        assertEquals(exp_res, res, 0.0001f);
+    }
+    @Test
+    void jaccardIndexTestDifferentArrays() {
+        int[] a = {0, 1, 3, 4, 5};
+        int[] b = {6, 7, 8, 9, 10};
+        int[] searchResults = {0, 0, 0, 0, 0};
+        double unique = 5.0;
+        double shared = 0.0;
+        double exp_res = shared/unique;
+
+        SimilarityFinder sf = new SimilarityFinder(new TestSequenceSearcher(searchResults));
+        double res = sf.calculateJackardSimilarity(a, b);
+        assertEquals(exp_res, res, 0.0001f);
+    }
+    @Test
+    void jaccardIndexTestEmptyArrays() {
+        int[] a = { };
+        int[] b = { };
+        int[] searchResults = { };
+        double unique = 1.0;
+        double shared = 1.0;
+        double exp_res = shared/unique;
+
+        SimilarityFinder sf = new SimilarityFinder(new TestSequenceSearcher(searchResults));
+        double res = sf.calculateJackardSimilarity(a, b);
+        assertEquals(exp_res, res, 0.0001f);
+    }
+    @Test
+    void sequenceSearcherTestIsFound() {
+        int[] a = {1, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] searchResults = {0, 1, 0, 0, 1, 1, 1, 1, 1, 0};
+        TestSequenceSearcher seqS = new TestSequenceSearcher(searchResults);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(searchResults[i] == 1, seqS.search(a[i], a).isFound());
         }
-        return SearchResult.builder().withPosition(-1).withFound(false).build();
-    };
-    private class jaccardGen {
-        public final int[] a;
-        public final int[] b;
-        public final int sample_size_a;
-        public final int sample_size_b;
-        public final int diff_pos;
-        public final double exp_res;
-        public jaccardGen(int sample_size_a, int sample_size_b, int diff_pos) {
-            this.sample_size_a = sample_size_a;
-            this.sample_size_b = sample_size_b;
-            this.diff_pos = diff_pos;
-            this.a = new int[sample_size_a];
-            this.b = new int[sample_size_b];
-
-            // Fill both sets with the same data
-            for (int i = 0; i < sample_size_a; i++) {
-                a[i] = i;
-            }
-            for (int i = 0; i < sample_size_b; i++) {
-                b[i] = i;
-            }
-            float shared = Math.min(sample_size_a, sample_size_b);
-            float unique = shared + Math.abs(sample_size_a - sample_size_b);
-            // Add an n-difference in the second set
-            for (int i = 0; i < sample_size_b; i++) {
-                if (i % diff_pos == 0) {
-                    b[i] = (i + 1)*(sample_size_a + sample_size_b);
-                    if (i < sample_size_a) {
-                        unique++;
-                        shared--;
-                    }
-                }
-            }
-            if (sample_size_a <= 0 || sample_size_b <= 0) {
-                if (sample_size_a == 0 && sample_size_b == 0) {
-                    exp_res = 1.0;
-                } else {
-                    exp_res = 0;
-                }
-
-            } else {
-                exp_res = (shared/unique);
-            }
+    }
+    @Test
+    void sequenceSearcherTestPosition() {
+        int[] a = {1, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] searchResults = {0, 1, 0, 0, 1, 1, 1, 1, 1};
+        TestSequenceSearcher seqS = new TestSequenceSearcher(searchResults);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals((searchResults[i] == 1) ? i : 0, seqS.search(a[i], a).getPosition());
         }
     }
-
     @Test
-    void jaccardIndexWithSmallSampleSizeAndSameSetSize() {
-        jaccardGen jt = new jaccardGen(10, 10, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithSmallSampleSizeAndDifferentSetSizeA() {
-        jaccardGen jt = new jaccardGen(12, 10, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithSmallSampleSizeAndDifferentSetSizeB() {
-        jaccardGen jt = new jaccardGen(10, 12, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBigSampleSizeAndSameSetSize() {
-        jaccardGen jt = new jaccardGen(100, 100, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBigSampleSizeAndDifferentSetSizeA() {
-        jaccardGen jt = new jaccardGen(200, 100, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBigSampleSizeAndDifferentSetSizeB() {
-        jaccardGen jt = new jaccardGen(100, 200, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBigDifferenceOfSetSizeA() {
-        jaccardGen jt = new jaccardGen(10000, 200, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBigDifferenceOfSetSizeB() {
-        jaccardGen jt = new jaccardGen(1, 10, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBothEmptyAArray() {
-        jaccardGen jt = new jaccardGen(0, 10, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBothEmptyBArray() {
-        jaccardGen jt = new jaccardGen(10, 0, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithBothEmptyArrays() {
-        jaccardGen jt = new jaccardGen(0, 0, 2);
-        SimilarityFinder simF = new SimilarityFinder(seqS);
-        double res = simF.calculateJackardSimilarity(jt.a, jt.b);
-        assertEquals(jt.exp_res, res, 0.0001);
-    }
-    @Test
-    void jaccardIndexWithNullSequenceSearcher() {
-        jaccardGen jt = new jaccardGen(12, 10, 2);
-        SimilarityFinder simF = new SimilarityFinder(null);
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            simF.calculateJackardSimilarity(jt.a, jt.b);
+    void sequenceSearcherTestOutOfBandsAccess() {
+        int[] a = {1, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] searchResults = {0, 0, 0};
+        TestSequenceSearcher seqS = new TestSequenceSearcher(searchResults);
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            for (int i = 0; i < searchResults.length + 1; i++) {
+                seqS.search(a[i], a);
+            }
         });
     }
 }
