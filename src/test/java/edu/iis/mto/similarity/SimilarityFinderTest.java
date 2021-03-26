826e1bc9996f0eb9144e1f1d3f3a3349e6ca3017
Dominik Snopek <222572@edu.p.lodz.pl>
2021-03-26 17:07:13 +0100

    two different sequences the same size

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -40,4 +40,17 @@ class SimilarityFinderTest {
         assertEquals(1,similarityFinder.calculateJackardSimilarity(seq1,seq2));
     }
 
+    @Test
+    public void twoDifferentSequence()
+    {
+        int [] seq1={1,2,3,4};
+        int [] seq2={9,2,8,4};
+        System.out.println(similarityFinder.calculateJackardSimilarity(seq1,seq2));
+
+        assertNotEquals(1,similarityFinder.calculateJackardSimilarity(seq1,seq2));
+    }
+
+
+
+
 }
