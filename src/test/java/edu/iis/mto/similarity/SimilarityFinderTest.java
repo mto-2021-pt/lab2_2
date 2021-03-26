105f17d2a6e8814375c38455ecd132363ce2cfdb
Julia <222596@edu.p.lodz.pl>
2021-03-26 23:59:29 +0100

    Test dla tych samych tablic

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,28 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> {
+        for(int i = 0; i < sequence.length; i++){
+            if(sequence[i] == elem){
+                return SearchResult.builder().withFound(true).withPosition(i).build();
+            }
+        }
+        return SearchResult.builder().withFound(false).build();
+    });
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testingTheSameArrays(){
+        int[] seq1 = {10,20,30,40};
+        int[] seq2 = {10,20,30,40};
+
+        assertEquals(1, similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
 
 }
