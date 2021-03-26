c6e1f128dcf44a2af29a594b706c3911ccd21985
Dominik Snopek <222572@edu.p.lodz.pl>
2021-03-26 16:57:12 +0100

    two empty sequence

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,34 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            for(int i=0;i<sequence.length;i++)
+            {
+                if(sequence[i]==elem)
+                {
+                    return SearchResult.builder().withFound(true).withPosition(i).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        }
+    });
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void twoEmptySequence() {
+
+        int [] seq1={};
+        int [] seq2={};
+
+        assertEquals(1.0d,similarityFinder.calculateJackardSimilarity(seq1,seq2));
     }
 
 }
