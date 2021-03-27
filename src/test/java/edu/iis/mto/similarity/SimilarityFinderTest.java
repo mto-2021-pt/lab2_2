0f88061ffa058070feef6793e47262ba73550d85
sadowski.dawid <222560@edu.p.lodz.pl>
2021-03-27 15:52:08 +0100

    Add test for two empty sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,27 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void similarityFinderShouldReturnOneForTwoEmptySequences() {
+        int[] states = {};
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        SequenceSearcherSimulator ss = new SequenceSearcherSimulator(states);
+        SimilarityFinder sf = new SimilarityFinder(ss);
+
+        assertEquals(1.0d, sf.calculateJackardSimilarity(seq1, seq2));
     }
 
 }
+
