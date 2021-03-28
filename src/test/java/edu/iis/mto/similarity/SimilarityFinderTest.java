16b526708b8ef702d13dda706383535474f2eb43
kamil <222574@edu.p.lodz.pl>
2021-03-28 17:34:47 +0200

    adding a test with two different numbers

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,23 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    SequenceSearcher sequenceSearcherTrue = (elem, sequence) -> SearchResult.builder().withFound(true).build();
+    SequenceSearcher sequenceSearcherFalse = (elem, sequence) -> SearchResult.builder().withFound(false).build();
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test1() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherFalse);
+
+        double result = similarityFinder.calculateJackardSimilarity(new int[]{5}, new int[]{1});
+
+        Assertions.assertEquals(0,result);
     }
 
 }
