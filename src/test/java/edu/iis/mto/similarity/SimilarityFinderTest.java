634c3d5285d4613b02f282c58a7a4afa35804f20
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-04-09 14:56:32 +0200

    pierwszy test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,34 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    public SearchResult searchResult;
+
+    SequenceSearcher sequenceSearcher = new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            for (int i = 0; i < sequence.length; i++) {
+                if (elem == sequence[i]) {
+                    return SearchResult.builder().withPosition(i).withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withPosition(-1).withFound(false).build();
+        }
+    };
+
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void returnOneSequencesAreEmptyTest(){
+        int[] array1 = {}, array2 = {};
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+        assertEquals(similarityFinder.calculateJackardSimilarity(array1, array2), 1);
     }
 
 }
